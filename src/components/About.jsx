787009d25b519ec42535b52ef07a45e5bb1ba00e
CarlosXViera <CarlosXviera@gmail.com>
2017-01-23 17:50:01 -0500

    Styling some-more

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,24 @@
 var React = require('react');
 
-var About = () => <h3>About Component</h3>;
+var About = () => {
+  return(
+    <div>
+      <h1 className="text-center">About</h1>
+      <p>
+        This is a weather application built on React.
+      </p>
+      <p>
+        Here are some tools I've used.
+      </p>
+      <ul>
+        <li><a href="#">blah</a></li>
+        <li><a href="#">blah</a></li>
+        <li><a href="#">blah</a></li>
+        <li><a href="#">blah</a></li>
+      </ul>
+    </div>
+
+  );
+};
 
 module.exports = About;
